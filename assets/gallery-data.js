6a43004cf8a94a56858ac4310ca3128a82afa828
JC <happyjac@gmail.com>
2026-09-06 15:03:22 +0800

    Update website 2026-09-06 15:03

--- assets/gallery-data.js
+++ assets/gallery-data.js
@@ -1,5 +1,145 @@
 window.GALLERY_PHOTOS = [
   {
+    "src": "assets/photos/web/2024-04-16_CRUISE_020-Strait-of-Malacca.jpg",
+    "width": 2400,
+    "height": 1600,
+    "caption": "2024-04-16_CRUISE_020-Strait of Malacca",
+    "subtitle": ""
+  },
+  {
+    "src": "assets/photos/web/2024-04-16_CRUISE_021-Strait-of-Malacca.jpg",
+    "width": 2400,
+    "height": 1600,
+    "caption": "2024-04-16_CRUISE_021-Strait of Malacca",
+    "subtitle": ""
+  },
+  {
+    "src": "assets/photos/web/2024-04-16_CRUISE_023-Strait-of-Malacca.jpg",
+    "width": 2400,
+    "height": 1600,
+    "caption": "2024-04-16_CRUISE_023-Strait of Malacca",
+    "subtitle": ""
+  },
+  {
+    "src": "assets/photos/web/2024-04-16_CRUISE_024-Strait-of-Malacca.jpg",
+    "width": 2400,
+    "height": 1600,
+    "caption": "2024-04-16_CRUISE_024-Strait of Malacca",
+    "subtitle": ""
+  },
+  {
+    "src": "assets/photos/web/2024-05-01_CA_032-Squamish.jpg",
+    "width": 2400,
+    "height": 1600,
+    "caption": "Squamish",
+    "subtitle": "2024-05-01_CA_032"
+  },
+  {
+    "src": "assets/photos/web/2024-05-01_CA_088-Kitsilano-Beach.jpg",
+    "width": 2400,
+    "height": 1600,
+    "caption": "Kitsilano Beach",
+    "subtitle": "2024-05-01_CA_088"
+  },
+  {
+    "src": "assets/photos/web/2024-05-02_CA_102-Lynn-Canyon-Park.jpg",
+    "width": 2400,
+    "height": 1600,
+    "caption": "Lynn Canyon Park",
+    "subtitle": "2024-05-02_CA_102"
+  },
+  {
+    "src": "assets/photos/web/2024-05-02_CA_103-Lynn-Canyon-Park.jpg",
+    "width": 2400,
+    "height": 1600,
+    "caption": "Lynn Canyon Park",
+    "subtitle": "2024-05-02_CA_103"
+  },
+  {
+    "src": "assets/photos/web/2024-05-02_CA_105-Lynn-Canyon-Park.jpg",
+    "width": 2400,
+    "height": 1600,
+    "caption": "Lynn Canyon Park",
+    "subtitle": "2024-05-02_CA_105"
+  },
+  {
+    "src": "assets/photos/web/2024-05-02_CA_106-Lynn-Canyon-Park.jpg",
+    "width": 2400,
+    "height": 3600,
+    "caption": "Lynn Canyon Park",
+    "subtitle": "2024-05-02_CA_106"
+  },
+  {
+    "src": "assets/photos/web/2024-05-02_CA_107-Lynn-Canyon-Park.jpg",
+    "width": 2400,
+    "height": 3600,
+    "caption": "Lynn Canyon Park",
+    "subtitle": "2024-05-02_CA_107"
+  },
+  {
+    "src": "assets/photos/web/2024-05-02_CA_113-Lynn-Canyon-Park.jpg",
+    "width": 2400,
+    "height": 1600,
+    "caption": "Lynn Canyon Park",
+    "subtitle": "2024-05-02_CA_113"
+  },
+  {
+    "src": "assets/photos/web/2024-05-02_CA_122-VanDusen-Botanical-Garden.jpg",
+    "width": 2400,
+    "height": 1600,
+    "caption": "VanDusen Botanical Garden",
+    "subtitle": "2024-05-02_CA_122"
+  },
+  {
+    "src": "assets/photos/web/2024-05-02_CA_134-VanDusen-Botanical-Garden.jpg",
+    "width": 2400,
+    "height": 3600,
+    "caption": "VanDusen Botanical Garden",
+    "subtitle": "2024-05-02_CA_134"
+  },
+  {
+    "src": "assets/photos/web/2024-05-08_US_108-Gulf-of-Alaska.jpg",
+    "width": 2400,
+    "height": 3600,
+    "caption": "Gulf of Alaska",
+    "subtitle": "2024-05-08_US_108"
+  },
+  {
+    "src": "assets/photos/web/2024-05-08_US_109-Gulf-of-Alaska.jpg",
+    "width": 2400,
+    "height": 1600,
+    "caption": "Gulf of Alaska",
+    "subtitle": "2024-05-08_US_109"
+  },
+  {
+    "src": "assets/photos/web/2024-05-08_US_110-Gulf-of-Alaska.jpg",
+    "width": 2400,
+    "height": 1600,
+    "caption": "Gulf of Alaska",
+    "subtitle": "2024-05-08_US_110"
+  },
+  {
+    "src": "assets/photos/web/2024-05-08_US_111-Gulf-of-Alaska.jpg",
+    "width": 2400,
+    "height": 1600,
+    "caption": "Gulf of Alaska",
+    "subtitle": "2024-05-08_US_111"
+  },
+  {
+    "src": "assets/photos/web/2024-05-09_US_112-Hubbard-Glacier.jpg",
+    "width": 2400,
+    "height": 1600,
+    "caption": "Hubbard Glacier",
+    "subtitle": "2024-05-09_US_112"
+  },
+  {
+    "src": "assets/photos/web/2024-05-09_US_126-Hubbard-Glacier.jpg",
+    "width": 2400,
+    "height": 1600,
+    "caption": "Hubbard Glacier",
+    "subtitle": "2024-05-09_US_126"
+  },
+  {
     "src": "assets/photos/web/2026-06-13_SG_02-East-Coast-Park.jpg",
     "width": 1960,
     "height": 1102,
@@ -33,47 +173,5 @@ window.GALLERY_PHOTOS = [
     "height": 692,
     "caption": "Maju Forest",
     "subtitle": "2026-09-06_SG_01"
-  },
-  {
-    "src": "assets/photos/web/Coogee-Beach-01.jpg",
-    "width": 2400,
-    "height": 1600,
-    "caption": "Coogee Beach 01",
-    "subtitle": ""
-  },
-  {
-    "src": "assets/photos/web/Coogee-Beach-02.jpg",
-    "width": 2400,
-    "height": 1600,
-    "caption": "Coogee Beach 02",
-    "subtitle": ""
-  },
-  {
-    "src": "assets/photos/web/Coogee-Beach-03.jpg",
-    "width": 2400,
-    "height": 1600,
-    "caption": "Coogee Beach 03",
-    "subtitle": ""
-  },
-  {
-    "src": "assets/photos/web/Uminonakamichi-01.jpg",
-    "width": 1960,
-    "height": 1306,
-    "caption": "Uminonakamichi 01",
-    "subtitle": ""
-  },
-  {
-    "src": "assets/photos/web/Uminonakamichi-02.jpg",
-    "width": 1960,
-    "height": 692,
-    "caption": "Uminonakamichi 02",
-    "subtitle": ""
-  },
-  {
-    "src": "assets/photos/web/Uminonakamichi-03.jpg",
-    "width": 1960,
-    "height": 692,
-    "caption": "Uminonakamichi 03",
-    "subtitle": ""
   }
 ];
